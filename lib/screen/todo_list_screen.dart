4723fa5a4d7c72813a47b1deea11d50e966458c3
otonari <otonari@manabi-labo.co.jp>
2020-05-11 14:38:26 +0900

    dynamicリンクを処理するメソッドを実装 クエリパラメーターからパスを取り出して画面遷移

--- lib/screen/todo_list_screen.dart
+++ lib/screen/todo_list_screen.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/widgets/todo_list.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 
-class TodoListScreen extends StatelessWidget {
+class TodoListScreen extends StatefulWidget {
+  @override
+  _TodoListScreenState createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen> {
+  @override
+  void initState() {
+    super.initState();
+    initLinkData();
+  }
+
+  void initLinkData() async {
+    var link = await FirebaseDynamicLinks.instance.getInitialLink();
+    handleLinkData(link);
+
+    FirebaseDynamicLinks.instance.onLink(
+        onSuccess: (PendingDynamicLinkData dynamicLink) async {
+      handleLinkData(dynamicLink);
+    });
+  }
+
+  void handleLinkData(PendingDynamicLinkData data) {
+    final Uri uri = data?.link;
+    print(uri.queryParameters["path"]);
+    Navigator.pushNamed(context, uri.queryParameters["path"]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
